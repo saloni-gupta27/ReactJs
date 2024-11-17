5b4c56dba2125ebaa67a5ebee0c0197de57c91f8
saloni-gupta27 <guptaSaloni.2016@gmail.com>
2024-11-17 23:33:41 +0530

    Understand props and States

--- react-concepts/src/App.js
+++ react-concepts/src/App.js
@@ -1,10 +1,75 @@
-import './App.css';
-import Navbar from './components/Navbar';
+import { useState } from "react";
+import "./App.css";
+// import About from "./components/About";
+import Form from "./components/Form";
+import Navbar from "./components/Navbar";
+import Alert from "./components/Alert";
 
 function App() {
+  let [mode, setMode]=useState("Light");
+  let [alert,setAlert] = useState(null);
+  let [color,setColor] = useState("");
+  
+  let showAlert =(message, type)=>{
+    setAlert({
+      msg: message,
+      type: type
+    });
+    setTimeout(()=>{
+      setAlert(null);
+    },1500)
+  }
+
+  let toggleMode = () => {
+    if(mode==='Dark'){
+    setMode('Light');
+    setColor("rgb(246 244 244);");
+    document.body.style.backgroundColor="#fff";
+    showAlert("Light mode is enabled", "success");
+    }
+    else{
+      setMode('Dark');
+      setColor("black");
+      document.body.style.backgroundColor="rgb(20, 49, 116)";
+    showAlert("Dark mode is enabled","dark");
+    }
+  }
+
+  let toggleRed =() =>{
+    if(mode==='Red'){
+      setMode('Light');
+      setColor("#530500;");
+      document.body.style.backgroundColor="#fff";
+      showAlert("Light mode is enabled", "success");
+      }
+      else{
+        setMode('Red');
+        setColor("#530500");
+        document.body.style.backgroundColor="#fc2b20";
+      showAlert("Red mode is enabled","danger");
+      }
+  }
+  let toggleGreen =() =>{
+    if(mode==='Green'){
+      setMode('Light');
+      setColor("rgb(246 244 244);");
+      document.body.style.backgroundColor="#fff";
+      showAlert("Light mode is enabled", "success");
+      }
+      else{
+        setMode('Green');
+        setColor("#093d09");
+        document.body.style.backgroundColor="green";
+      showAlert("Green mode is enabled","success");
+      }
+  }
   return (
     <>
-     <Navbar title="TextUtils" item1="Home" item2="About"/>
+      <Navbar title="TextUtils" item1="Home" item2="About" mode={mode} toggleMode={toggleMode} toggleRed={toggleRed}  toggleGreen={toggleGreen} color={color}/>
+      <Alert alert={alert}/>
+        <Form  heading="Enter the text here" mode={mode} alert={alert} showAlert={showAlert} color={color}/>
+        {/* <About/> */}
+    
     </>
   );
 }
---
+++ react-concepts/src/components/Form.js
@@ -0,0 +1,90 @@
+import React from "react";
+import { useState } from "react";
+
+export default function Form(props) {
+  let [text, setText] = useState("");
+
+  let handleChange = (event) => {
+    setText(event.target.value);
+  };
+  let handleUpperCaseConversion = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert("Converted to UpperCase","success");
+  };
+  let handleLowerCaseConversion = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+    props.showAlert("Converted to LowerCase","success");
+  };
+  let handleRemoveSpaces = () => {
+    let newText = text.split(" ").join("");
+    setText(newText);
+    props.showAlert("Spaces Removed","success");
+  };
+  let handleReverseText = () => {
+    let newText = text.split("").reverse().join("");
+    setText(newText);
+    props.showAlert("Text Reversed","success");
+  };
+  let clearText = () => {
+    setText("");
+    props.showAlert("Text cleared","success");
+  };
+  let copyText = () => {
+    let newText= document.getElementById("myBox");
+    newText.select();
+    navigator.clipboard.writeText(newText.value);
+    props.showAlert("Copied to clipboard","success");
+  };
+
+  return (
+    <>
+    <div className="container my-3" style={{color: props.mode==="Light"? 'black': 'white'}}>
+      <h1>{props.heading}</h1>
+      <div className="mb-3">
+        <label htmlFor="myBox" className="form-label"></label>
+        <textarea
+          className="form-control"
+          id="myBox"
+          rows="8"
+          value={text}
+          onChange={handleChange}
+          style={{backgroundColor: props.mode==="Dark"? '#d1d1d1': 'white'}}
+        ></textarea>
+      </div>
+      <button
+        className="btn btn-primary mx-2"
+        onClick={handleUpperCaseConversion}
+        style={{backgroundColor: props.mode!== 'Light' ?  props.color : '', borderColor: props.mode!== 'Light' ?  props.color : ''}}
+      >
+        Convert to UpperCase
+      </button>
+      <button className="btn btn-primary mx-2" onClick={handleLowerCaseConversion} style={{backgroundColor: props.mode!== 'Light' ?  props.color : '', borderColor: props.mode!== 'Light' ?  props.color : ''}}>
+        Convert to LowerCase
+      </button>
+      <button className="btn btn-primary mx-2" onClick={handleRemoveSpaces} style={{backgroundColor: props.mode!== 'Light' ?  props.color : '', borderColor: props.mode!== 'Light' ?  props.color : ''}}>
+        Remove Spaces
+      </button>
+      <button className="btn btn-primary mx-2" onClick={handleReverseText} style={{backgroundColor: props.mode!== 'Light' ?  props.color : '', borderColor: props.mode!== 'Light' ?  props.color : ''}}>
+        Reverse Text
+      </button>
+      <button className="btn btn-primary mx-2" onClick={clearText} style={{backgroundColor: props.mode!== 'Light' ?  props.color : '', borderColor: props.mode!== 'Light' ?  props.color : ''}}>
+        Clear Text
+      </button>
+      <button className="btn btn-primary mx-2" onClick={copyText} style={{backgroundColor: props.mode!== 'Light' ?  props.color : '', borderColor: props.mode!== 'Light' ?  props.color : ''}}>
+        Copy Text
+      </button>
+      <div className="container my-3">
+        <h2>Your text summary</h2>
+        <p>
+          {text.split(" ").length} words {text.length} characters.
+        </p>
+        <p>{0.008 * text.split(" ").length} minutes read.</p>
+        <h2>Preview</h2>
+        <p>{text.length<=0?"Enter the text above to see preview":text}</p>
+      </div>
+      </div>
+    </>
+  );
+}
